840c27be91c6f589c3f02387fa2d7d10a23c55a3
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-15 02:01:04 +0530

    securitysearchdelete

--- src/app/search-security/search-security.component.ts
+++ src/app/search-security/search-security.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { ApiService } from '../api.service';
 
 @Component({
   selector: 'app-search-security',
@@ -6,5 +8,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./search-security.component.css']
 })
 export class SearchSecurityComponent {
-
+  
+  constructor(private api:ApiService,private route:Router){}
+  seccode=""
+  searchdata:any=[]
+  readValues=()=>{
+    let data:any={
+      "seccode":this.seccode
+    }
+    console.log(data)
+    this.api.searchSecurity(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if (response.length==0) {
+  
+          alert("invalid security code")
+          
+        } else {
+          this.searchdata=response
+        }
+      }
+    )
+  }
+  deletebuttonclick=(id:any)=>{
+  
+    let data:any={
+      "id":id
+    }
+    this.api.deleteSecurity(data).subscribe(
+      (response:any)=>{
+        console.log(response)
+        if (response.status=="success") {
+  
+          alert("Deleted");
+          this.route.navigate(['/viewsecurity'])
+        } 
+        else {
+        
+          alert("invalid input");
+        }
+      }
+    )
+  
+  }
+  
 }
