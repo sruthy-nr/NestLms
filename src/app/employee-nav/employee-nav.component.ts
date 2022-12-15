d22d6f38c7ec20f06e87d88ea6fc12d9a76447f3
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-15 22:51:13 +0530

    employeenav

---
+++ src/app/employee-nav/employee-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-employee-nav',
+  templateUrl: './employee-nav.component.html',
+  styleUrls: ['./employee-nav.component.css']
+})
+export class EmployeeNavComponent {
+
+}
